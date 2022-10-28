797988a63d2545145c891f6a03e9eb2611bffd07
Vladimir Pavlov <91782115+Popeye80@users.noreply.github.com>
2022-10-28 14:42:46 +0300

    28

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
 const morgan = require('morgan');
 require('dotenv').config();
 
+const PORT = process.env.PORT || 8081;
+
 const app = express();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  console.log('new client connected!');
+  socket.on('CHAT_MESSAGE', ({message, username}) => {
+    io.emit('CHAT_UPDATE', {message, username});
+  });
+});
 
 const {connectMongo} = require('./src/db/connection');
 const {postsRouter} = require('./src/routers/postsRouter');
@@ -11,9 +22,8 @@ const {authRouter} = require('./src/routers/authRouter');
 const {filesRouter} = require('./src/routers/filesRouter');
 const {errorHandler} = require('./src/helpers/apiHelpers');
 
-const PORT = process.env.PORT || 8081;
-
 app.use(express.json());
+app.use(express.static('build'));
 app.use(morgan('tiny'));
 app.use('/api/posts', postsRouter);
 app.use('/api/auth', authRouter);
@@ -23,7 +33,7 @@ app.use(errorHandler);
 const start = async () => {
   try {
     await connectMongo();
-    app.listen(PORT, (err) => {
+    server.listen(PORT, (err) => {
       if (err) console.log('Error at server launch:', err);
       console.log(`Server works at port ${PORT}!`);
     });
